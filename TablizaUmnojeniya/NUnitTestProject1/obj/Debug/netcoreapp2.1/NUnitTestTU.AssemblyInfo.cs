56606261ac339a55ebf2dc2cb756e8782d10b829
Peter <real3985@mail.ru>
2020-02-06 12:01:33 +0300

    New

--- TablizaUmnojeniya/NUnitTestProject1/obj/Debug/netcoreapp2.1/NUnitTestTU.AssemblyInfo.cs
+++ TablizaUmnojeniya/NUnitTestProject1/obj/Debug/netcoreapp2.1/NUnitTestTU.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("TablizaUmnojenya_Tests")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("NUnitTestTU")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("TablizaUmnojenya_Tests")]
-[assembly: System.Reflection.AssemblyTitleAttribute("TablizaUmnojenya_Tests")]
+[assembly: System.Reflection.AssemblyProductAttribute("NUnitTestTU")]
+[assembly: System.Reflection.AssemblyTitleAttribute("NUnitTestTU")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
